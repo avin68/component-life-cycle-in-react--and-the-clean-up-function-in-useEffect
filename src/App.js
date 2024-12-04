af0beccff4f0c939b2864aaa7b64c33dc84a2134
avin <esmaili_avin@yahoo.com>
2024-12-04 12:25:29 +0330

    1st commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,95 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [input, setInput] = useState("a");
+
+  const clickHandler = () => {
+    setCount(count + 1);
+  };
+
+  ////////////////////use effect without second argumnet////////
+
+  // useEffect(()=>{
+  //   console.log('avin');
+
+  // })
+
+  /////////use effect with a second argumnet which is an empty array////////
+
+  // useEffect(()=>{
+  //   console.log('avin');
+
+  // } , [])
+
+  //////////////use effect with a second argumnet which shows a state////////
+
+  // useEffect(()=>{
+  //   console.log('avin');
+
+  // } , [count])
+
+  ////////use effect with a second argumnet which shows another state////////
+
+  // useEffect(()=>{
+  //   console.log('avin');
+
+  // } , [input])
+
+  /////////////////////////////////////clean up function/////////
+
+  // useEffect(()=>{
+  //   console.log('avin' , count);
+
+  //   return ()=> {
+  //     console.log('sali' , count)
+  //   }
+
+  // })
+
+  /////////////////////////////////////clean up function/////////
+
+  // useEffect(()=>{
+  //   console.log('avin', count);
+
+  //   return ()=> {
+  //     console.log('sali' , count)
+  //   }
+
+  // },[])
+
+  /////////////////////////////////////clean up function/////////
+
+  // useEffect(()=>{
+  //   console.log('avin', count);
+  //       return ()=> {
+  //     console.log('sali' , count)
+  //   }
+
+  // } , [count])
+
+  /////////////////////////////////////clean up function/////////
+
+  useEffect(() => {
+    console.log("avin", count, input);
+    return () => {
+      console.log("sali", count, input);
+    };
+  }, [input, count]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <h1>count : {count}</h1>
+      <button onClick={clickHandler}>increase</button>
+      <hr />
+      <input
+        type="text"
+        value={input}
+        onChange={(e) => setInput(e.target.value)}
+      />
+      <h1>input value: {input}</h1>
+      <hr />
+    </>
   );
 }
 
